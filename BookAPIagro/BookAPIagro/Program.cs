b493b4834aff55c21b897e52f9fab8f2a157e70f
diogomene <diogodelimamenezes15@gmail.com>
2024-01-19 19:25:02 -0400

    Tranfering jsonPath to appsettings.json

--- BookAPIagro/BookAPIagro/Program.cs
+++ BookAPIagro/BookAPIagro/Program.cs
@@ -2,17 +2,14 @@ using BookAPIagro.UseCases;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+//Decide por deixar o swagger público, mesmo para produção
 app.UseSwagger();
 app.UseSwaggerUI();
 
--- BookAPIagro/BookAPIagro/UseCases/DataBootstrapper.cs
+++ BookAPIagro/BookAPIagro/UseCases/DataBootstrapper.cs
@@ -4,10 +4,18 @@ namespace BookAPIagro.UseCases
 {
     public class DataBootstrapper
     {
+        private static string? getDataStoragePath() {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            IConfigurationRoot confRoot = configurationBuilder.AddJsonFile("appsettings.json").Build();
+            return confRoot.GetValue<string>("DataStorageFilePath");
+
+        }
+
         public static void Bootstrap()
         {
-            string jsonPath = "DataAccess/books.json";
+            string? jsonPath = getDataStoragePath();
             BookStore bookStore = BookStore.GetInstance();
+            if (jsonPath == null) return;
             bookStore.StoreList = new BookStoreList(BooksLoader.LoadBooks(jsonPath).ToList());
         }
     }
